d03188f61c8bb20abd15abe7b7e12a57c3fb8be2
Nishpowale <88265620+Nishpowale@users.noreply.github.com>
2022-06-22 19:52:04 +0530

    Add files via upload

---
+++ String Palindrome.py
@@ -0,0 +1,10 @@
+def isPalindrome(S):
+		#count=0
+        if S == str(S[::-1]):
+            print(1)
+        else :
+            print(0)
+String= "abba"
+String1="abc"
+isPalindrome(String)
+isPalindrome(String1)
